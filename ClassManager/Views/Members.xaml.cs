1c7b29f26694e821b179ed6318d189702078adce
linzibo <673418747@qq.com>
2016-05-07 00:20:03 +0800

    add split UI and remove the useage of page"MemberOfClass"

--- ClassManager/Views/Members.xaml.cs
+++ ClassManager/Views/Members.xaml.cs
@@ -15,14 +15,14 @@ using Windows.UI.Xaml.Navigation;
 
 // “空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=234238 上有介绍
 
-namespace ClassManager
+namespace ClassManager.Views
 {
 	/// <summary>
 	/// 可用于自身或导航至 Frame 内部的空白页。
 	/// </summary>
-	public sealed partial class Organization : Page
+	public sealed partial class Members : Page
 	{
-		public Organization ()
+		public Members ()
 		{
 			this.InitializeComponent();
 		}

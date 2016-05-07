e98d8b7c7901b9fc620455ca2982b3b0f23d156c
linzibo <673418747@qq.com>
2016-05-07 21:45:24 +0800

    update homework page

--- ClassManager/Views/Homeworks.xaml.cs
+++ ClassManager/Views/Homeworks.xaml.cs
@@ -1,18 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
+using System.Collections.ObjectModel;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
-
+using ClassManager.Model;
+using ClassManager.ViewModels;
+using Windows.UI.Xaml;
 // “空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=234238 上有介绍
 
 namespace ClassManager.Views
@@ -22,9 +16,37 @@ namespace ClassManager.Views
 	/// </summary>
 	public sealed partial class Homeworks : Page
 	{
+		private OrganizationViewModel OVM;
+		private SelectItemOfHomeworks SelectedItem;
 		public Homeworks ()
 		{
 			this.InitializeComponent();
+		}
+
+		protected override void OnNavigatedTo (NavigationEventArgs e)
+		{
+			OVM.initialOVM((string)e.Parameter);
+		}
+
+
+	}
+
+	public class SelectItemOfHomeworks
+	{
+		public SelectItemOfHomeworks ()
+		{
+			Users = new ObservableCollection<User>();
+		}
+		private ObservableCollection<User> users;
+		public ObservableCollection<User> Users {
+			get {
+				return users;
+			}
+			set {
+				foreach (User item in value) {
+					users.Add(item);
+				}
+			}
 		}
 	}
 }
